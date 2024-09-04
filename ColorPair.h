23f3c4814a4d8a63635541d737e90ba195747b4e
“Khandelwal <Gaurav.Khandelwal@philips.com>
2024-09-04 16:31:50 +0530

    initial

---
+++ ColorPair.h
@@ -0,0 +1,28 @@
+#include <string>
+#include "ColorCoder.h"
+
+namespace TelCoColorCoder
+{
+
+    class ColorPair {
+    private:
+        MajorColor majorColor;
+        MinorColor minorColor;
+    public:
+        ColorPair(MajorColor major, MinorColor minor) :
+            majorColor(major), minorColor(minor)
+        {}
+        MajorColor getMajor() {
+            return majorColor;
+        }
+        MinorColor getMinor() {
+            return minorColor;
+        }
+        std::string ToString() {
+            std::string colorPairStr = MajorColorNames[majorColor];
+            colorPairStr += " ";
+            colorPairStr += MinorColorNames[minorColor];
+            return colorPairStr;
+        }
+    };
+}
